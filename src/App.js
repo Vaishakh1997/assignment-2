2262338b127625b9e11350fd7dc4707645910c6e
Vaishakh1997 <vaishakhnetla@gmail.com>
2020-05-13 19:09:03 +0530

    All carousel changes are done.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, BrowserRouter as Router, Link, Switch} from "react-router-dom";
+import { Route, BrowserRouter as Router, Switch} from "react-router-dom";
 import './App.css';
 import Albums from './component/Albums'
 import Photos from './component/Photos'
--- src/component/Photos.js
+++ src/component/Photos.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 const axios = require('axios');
-
 class Photos extends Component {
     state = {
-        photosData: [],
         imageURL: '',
-        allImageURL: []
+        allImageURL: [],
     }
+
     componentDidMount() {
         axios({
             method: 'get',
@@ -14,36 +13,76 @@ class Photos extends Component {
         })
             .then(response => {
                 var allImageURL = []
-                response.data.map(photo => {
-                    allImageURL.push(photo.url)
-                })
-                this.setState({ photosData: response.data, allImageURL: allImageURL })
+                response.data.map(photo => allImageURL.push(photo.url))
+                this.setState({allImageURL: allImageURL })
             })
-            .catch(error => {
-                console.error(error)
-            })
+            .catch(error => console.error(error))
     }
-  
+
+    prevSlide = () => {
+        let targetID;
+        if(this.props.location.hash)
+            targetID = this.props.location.hash
+        else
+            targetID = "#1"
+        while (targetID.charAt(0) === '#')
+            targetID = targetID.slice(1);
+        if(targetID !== "1")
+            targetID = parseInt(targetID)-1
+        this.props.history.push(`#${targetID}`)
+        window.location = `/albums/${this.props.match.params.albumId}#${targetID}`
+    }
+
+    nextSlide = () => {
+        let targetID;
+        if(this.props.location.hash)
+            targetID = this.props.location.hash
+        else
+            targetID = "#1"
+        while (targetID.charAt(0) === '#')
+            targetID = targetID.slice(1);
+        targetID = parseInt(targetID)+1
+        this.props.history.push(`#${targetID}`)
+        window.location = `/albums/${this.props.match.params.albumId}#${targetID}`
+    }
+    
     render() {
+        let prev="<"
+        let next=">"
+
         return (
             <React.Fragment>
                 <div className="Photos-list">
-                    <div class="slider">
-                        
-                        <div class="slides">
-                            {this.state.allImageURL.map((url, index)=>{
-                                return  <div id={index+1}>
-                                            <img style={{width:'100%',height:'100%'}} src={url} ></img>
-                                        </div>
+                    <button className="prev" onClick={this.prevSlide}>
+                        {prev}
+                    </button>
+
+                    <div className="slider">
+                        <div className="slides">
+                            {this.state.allImageURL.map((url, index) => {
+                                return (
+                                    <div id={index + 1} key={index}>
+                                        <img
+                                            style={{ width: "100%", height: "100%" }}
+                                            src={url}
+                                            alt={index}
+                                        ></img>
+                                    </div>
+                                );
                             })}
                         </div>
 
-                        {this.state.allImageURL.map((url, index)=>{
-                            let targetID = `#${index+1}`
-                            return <a href={targetID}>{index+1}</a>
-                        })}                        
+                        {this.state.allImageURL.map((url, index) => {
+                            let targetID = `#${index + 1}`;
+                            return (
+                                <a href={targetID} key={targetID}></a>
+                            );
+                        })}
                     </div>
-                    
+
+                    <button className="next" onClick={this.nextSlide}>
+                        {next}
+                    </button>
                 </div>
             </React.Fragment>
         );
