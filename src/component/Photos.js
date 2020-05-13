e6256a764bd391b8ed28b379ceae044c29ea8674
Vaishakh1997 <vaishakhnetla@gmail.com>
2020-05-13 16:21:48 +0530

    image carousel is working.

--- src/component/Photos.js
+++ src/component/Photos.js
@@ -2,52 +2,52 @@ import React, { Component } from 'react';
 const axios = require('axios');
 
 class Photos extends Component {
-    state = { 
+    state = {
         photosData: [],
         imageURL: '',
-        allImageURL:[]
-     }
-    componentDidMount(){
+        allImageURL: []
+    }
+    componentDidMount() {
         axios({
             method: 'get',
             url: `https://jsonplaceholder.typicode.com/photos?albumId=${this.props.match.params.albumId}`
-          })
-            .then(response=> {
+        })
+            .then(response => {
                 var allImageURL = []
-                response.data.map(photo=>{
+                response.data.map(photo => {
                     allImageURL.push(photo.url)
                 })
-                this.setState({photosData:response.data, allImageURL:allImageURL})
+                this.setState({ photosData: response.data, allImageURL: allImageURL })
             })
-            .catch(error=>{
+            .catch(error => {
                 console.error(error)
             })
-            .then(res=>{
-                this.carousel()
-            })
     }
-    carousel = () =>{
-        this.state.allImageURL.map(url=>{
-            setInterval(()=>{
-                this.setState({imageURL: url})
-            }, 5000);
-        })
-    }
-    render() { 
-        return ( 
+  
+    render() {
+        return (
             <React.Fragment>
                 <div className="Photos-list">
-                    {/* {this.state.photosData.map(photo=>{
-                        return( */}
-                            <div className="photo">
-                                <img style={{width:'100%',height:'100%'}} src={this.state.imageURL}></img>
-                            </div>
-                        {/* )
-                    })} */}
+                    <div class="slider">
+                        
+                        <div class="slides">
+                            {this.state.allImageURL.map((url, index)=>{
+                                return  <div id={index+1}>
+                                            <img style={{width:'100%',height:'100%'}} src={url} ></img>
+                                        </div>
+                            })}
+                        </div>
+
+                        {this.state.allImageURL.map((url, index)=>{
+                            let targetID = `#${index+1}`
+                            return <a href={targetID}>{index+1}</a>
+                        })}                        
+                    </div>
+                    
                 </div>
             </React.Fragment>
-         );
+        );
     }
 }
- 
+
 export default Photos;
