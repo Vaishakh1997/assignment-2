dbf0d3684765d888b13c81b2338ba8ece8697faa
Vaishakh1997 <vaishakhnetla@gmail.com>
2020-05-20 20:12:28 +0530

    Working on Async functions

---
+++ src/Fetch-API/api-fetch.test.js
@@ -0,0 +1,64 @@
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import * as types from '../actions/index'
+import { fetchAlbums, fetchPhotos } from './api-fetch'
+import fetchMock from 'fetch-mock'
+
+const middlewares = [thunk]
+const mockStore = configureMockStore(middlewares)
+
+
+describe('async Fetch albums actions', () => {
+
+  afterEach(() => {
+    fetchMock.restore()
+  })
+
+  it("handles changing a Albums status", async () => {
+    const store = mockStore();
+    store.dispatch(fetchAlbums());
+    expect(await getAction(store, "FETCH_ALBUMS_REQUEST")).toEqual({type: types.FETCH_ALBUMS_REQUEST});
+    // expect(await getAction(store, "FETCH_ALBUMS_SUCCESS")).toEqual({
+    //     type: types.FETCH_ALBUMS_SUCCESS
+    // });
+  });
+  
+})
+
+
+
+describe('async Fetch photos actions', () => {
+
+    afterEach(() => {
+      fetchMock.restore()
+    })
+  
+    it("handles changing a photos status", async () => {
+      const store = mockStore();
+      store.dispatch(fetchPhotos());
+      expect(await getAction(store, "FETCH_PHOTOS_REQUEST")).toEqual({type: types.FETCH_PHOTOS_REQUEST});
+
+    });
+    
+  })
+
+
+
+
+
+
+function findAction(store, type) {
+    return store.getActions().find(action => action.type === type);
+  }
+  
+function getAction(store, type) {
+    const action = findAction(store, type);
+    if (action) return Promise.resolve(action);
+  
+    return new Promise(resolve => {
+      store.subscribe(() => {
+        const action = findAction(store, type);
+        if (action) resolve(action);
+      });
+    });
+  }
